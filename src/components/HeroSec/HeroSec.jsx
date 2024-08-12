b27da1dcb43a70d6c66769a030176e8472711ef1
Jai Singh <88092614+JaiSinghRajput@users.noreply.github.com>
2024-08-12 09:29:40 +0530

    Update HeroSec.jsx

--- src/components/HeroSec/HeroSec.jsx
+++ src/components/HeroSec/HeroSec.jsx
@@ -31,7 +31,7 @@ function HeroSec() {
             </svg>
           </NavLink>
           <NavLink
-            to="/contact-info"
+            to="/Contact"
             className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
           >
             Contact Info
